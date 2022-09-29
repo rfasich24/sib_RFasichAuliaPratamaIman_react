316400a3ebd1db9e426966c9b2e5b66a692f1466
rfasich24 <pratama.fasih24@gmail.com>
2022-09-29 19:50:06 +0700

    sesi12

---
+++ sesi_12/materi-next/pages/users-ssr.js
@@ -0,0 +1,31 @@
+export default function Users(props) {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.users?.map((user) => (
+                    <tr key={user.id}>
+                        <td>{user.id}</td>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+}
+
+export async function getServerSideProps(context) {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+
+    return {
+        props: { users },
+    };
+}
